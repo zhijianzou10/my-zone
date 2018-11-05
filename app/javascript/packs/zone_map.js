b65eae32f5410c018915b00493db7fed1575d9f9
zhijianzou <justinchow10@hotmail.com>
2018-11-05 01:42:42 +0100

    creation zone view done with dynamic map

--- app/javascript/packs/zone_map.js
+++ app/javascript/packs/zone_map.js
@@ -1,22 +1,36 @@
-var map = L.map('creation_zone', { drawControl: true }).setView([48.856614, 2.3522219], 13)
+var map = L.map('creation_zone', { drawControl: false }).setView([48.856614, 2.3522219], 16)
 var ggRoadmap = new L.Google('ROADMAP');
 map.addLayer(ggRoadmap);
+var geocoder;
+geocoder = new google.maps.Geocoder();
 
-var drawnItems = new L.FeatureGroup();
-
- var drawControl = new L.Control.Draw({
-     edit: {
-         featureGroup: drawnItems
-     }
- });
- map.addLayer(drawnItems);
- map.on(L.Draw.Event.CREATED, function (event) {
-      var layer = event.layer;
-      var coordinates = layer.getLatLng();
-      console.log(coordinates.lat)
-      console.log(layer.getRadius())
-      drawnItems.addLayer(layer);
+var update = function() {
+  map.eachLayer(function(layer){
+    if (layer != ggRoadmap){
+      layer.remove();
+    }
   });
+
+  var input_radius = parseInt($("input[name='zone[radius]']").val());
+  var input_address = String($("textarea[name='zone[address]']").val());
+
+  if (input_address != "" && isNaN(input_radius) === false && input_radius < 500) {
+    geocoder.geocode( { 'address': input_address}, function(results, status) {
+      if (status == 'OK') {
+        var coordinates = [results[0].geometry.location.lat(),results[0].geometry.location.lng()];
+        L.circle(coordinates, {radius: input_radius}).addTo(map);
+        map.setView(coordinates, 16);
+      } else {
+        alert('Please enter a valid address');
+      }
+    });
+  } else if (isNaN(input_radius) === false && input_radius >= 500) {
+    alert('Please enter a radius less than 500 meters');
+  };
+};
+update();
+$('form').change(update);
+
 
 
 
