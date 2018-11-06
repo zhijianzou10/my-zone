ee18177f17316dbe92012918e57e36b09ce56cef
zhijianzou <justinchow10@hotmail.com>
2018-11-06 12:44:23 +0100

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_131946) do
+ActiveRecord::Schema.define(version: 2018_11_06_110249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_131946) do
     t.integer "gender", default: 0
     t.string "avatar_url"
     t.boolean "is_admin", default: false
+    t.string "background_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
