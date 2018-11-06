9ffd886a8f97324a2284fe3af97c7ca6731f1d52
BENOIT LEGENDRE <benoit.legendre.bba@edhec.com>
2018-11-06 17:01:45 +0100

    gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-InterestTag.create(title: 'Sport', photo:'interests/fitness.jpg')
-InterestTag.create(title: 'Photography', photo:'interests/photography.jpg')
-InterestTag.create(title: 'Tech', photo:'interests/technology.jpg')
-InterestTag.create(title: 'Cooking', photo:'interests/cooking.jpg')
-InterestTag.create(title: 'Gardening', photo:'interests/gardening.jpg')
-InterestTag.create(title: 'Decoration', photo:'interests/decoration.jpg')
-InterestTag.create(title: 'Reading', photo:'interests/reading.jpg')
-InterestTag.create(title: 'Travel', photo:'interests/travel.jpg')
-InterestTag.create(title: 'Video games', photo:'interests/videogames.jpg')
+# InterestTag.create(title: 'Sport', photo:'interests/fitness.jpg')
+# InterestTag.create(title: 'Photography', photo:'interests/photography.jpg')
+# InterestTag.create(title: 'Tech', photo:'interests/technology.jpg')
+# InterestTag.create(title: 'Cooking', photo:'interests/cooking.jpg')
+# InterestTag.create(title: 'Gardening', photo:'interests/gardening.jpg')
+# InterestTag.create(title: 'Decoration', photo:'interests/decoration.jpg')
+# InterestTag.create(title: 'Reading', photo:'interests/reading.jpg')
+# InterestTag.create(title: 'Travel', photo:'interests/travel.jpg')
+# InterestTag.create(title: 'Video games', photo:'interests/videogames.jpg')
+
+
+# interest = ["Sport", "Photography", "Tech", "Cooking", "Gardening", "Decoration", "Reading", "Travel", "Video games"]
+# puts 'Creating 100 fake profile...'
+# 100.times do |user|
+#  user_profile = User.create!(
+#     email:
+#     password:
+#    firstname: Faker::Name.first_name,
+#    lastname: Faker::Name.last_name,
+#    age: rand(10..65).to_i,
+#    gender: rand(0..2).to_i,
+#    address: Faker::Address.city,
+#    interest_tags: interests.sample(1)
+#    )
+# end
