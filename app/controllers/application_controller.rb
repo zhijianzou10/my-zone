bbd5b90fd52dff93a72a4a3686a3fefc86ffebf0
zhijianzou <justinchow10@hotmail.com>
2018-11-08 18:37:45 +0100

    updated transparency on charts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :gender])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :gender, :birth_year])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :gender, :birth_year])
   end
 
   # def after_sign_in_path_for(resource)
